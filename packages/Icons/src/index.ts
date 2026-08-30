b3ed1ca11fdfcf909fb28cad7d047834d1f595ff
Kunal-Rathore <kunalworkspace111@gmail.com>
2026-08-30 23:00:00 +0530

    feat(icons): extract shared @repo/icons package into workspace
    - Create new @repo/icons workspace package under packages/Icons
    - Add initial icon component collection (Chatbot, Github, Moon, Search, Sun, X)
    - Link @repo/icons dependency in web app package.json and update lockfile
    - Update repository README documentation

---
+++ packages/Icons/src/index.ts
@@ -0,0 +1,16 @@
+import { ChatbotIcon } from "./lib/Icons_store/Chatbot";
+import { GithubIcon } from "./lib/Icons_store/Github";
+import { MoonIcon } from "./lib/Icons_store/Moon";
+import { SearchIcon } from "./lib/Icons_store/Search";
+import { SunIcon } from "./lib/Icons_store/Sun";
+import { XIcon } from "./lib/Icons_store/X";
+
+
+export const IconsStore = [
+    ChatbotIcon,
+    GithubIcon,
+    MoonIcon,
+    SearchIcon,
+    SunIcon,
+    XIcon,
+];
